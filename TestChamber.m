ae1a83f9659c4e1798412a121ddec7e3e88a4ab8
szymsioH <01169288@pw.edu.pl>
2024-12-13 14:03:21 +0100

    fixed TestChamber (not inlcuding os of correlation)

--- TestChamber.m
+++ TestChamber.m
@@ -15,13 +15,12 @@ sig_time_new = numel(signal_dvbt)/50;
 signal = signal_dvbt(int64(numel(signal_dvbt)/3):int64(numel(signal_dvbt)/3 + sig_time_new - 1));
 
 %przesunięcie sygnałów
-distance = 10000; %odległość w m
+distance = 1000; %odległość w m
 delay = distance/c; %opóźnienie w s
 
 os_type = 0; %rodzaj interpolacji
 os_value = 1;
-co_value = 1;
-fs = fs * os_value;
+co_value = 5;
 
 dev_iter = 50; %iteracje
 
@@ -67,6 +66,7 @@ switch os_type
     otherwise
         os_value = 1;
 end
+fs = fs * os_value;
 
 
 
@@ -82,13 +82,18 @@ SNR_eqalizer2 = snr2_check;
 
 % SNR_list1 = -30:2:-24;
 % SNR_list2 = -23.5:0.5:0.5;
-% SNR_list3 = 1:1:11;
-% SNR_list4 = 11.5:2:15.5;
+% SNR_list3 = 1:1:4;
+% SNR_list4 = 5:5:20;
 
-SNR_list1 = -5:5:5;
-SNR_list2 = 10:5:20;
-SNR_list3 = 25:5:30;
-SNR_list4 = 35:5:160;
+SNR_list1 = -30:2:-24;
+SNR_list2 = -23.5:0.5:0.5;
+SNR_list3 = 1:1:4;
+SNR_list4 = 5:5:160;
+
+iter_index = 0;
+
+allerr_list = zeros(3, numel([SNR_list1, SNR_list2, SNR_list3, SNR_list4])); %wiersze: 1 - mean, 2 - std, 3 - rms
+SNRs_list = zeros(1, numel(allerr_list(1, :)));
 
 for n = 1:4
     switch n
@@ -102,9 +107,10 @@ for n = 1:4
             SNR_list = SNR_list4;
     end
 
-    devs_list = zeros(1, numel(SNR_list)); %1 wiersz - dewiacje, 2 wiersz - błąd średni
-
     for j = SNR_list
+        
+        iter_index = iter_index + 1;
+
 %         SNR1 = SNR_eqalizer1 + j;
 %         SNR2 = SNR_eqalizer2 + j;
         SNR1 = j;
@@ -151,52 +157,71 @@ for n = 1:4
                 max_lag = -rangecorr(corrval_itp == max(corrval_itp));
             end
             
-            est_delay = double(max_lag)/double(fs);
+            est_delay = double(max_lag)/(double(fs)); % wyestymowane opóźnienie w s
             
             est_errors(i) = abs(delay - est_delay); %błąd estymaty w sekundach
         end
     
-    % figure('Name', 'Errors [s]')
-    % plot(est_errors)
-    % 
-    % figure('Name', 'Errors in samples')
-    % plot(lag_errors)
-    
-    %     mean_lag = mean(lag_errors);
-    %     dev_lag = std(lag_errors);
-        
-        mean_error = mean(est_errors);
-        devs_list(1, SNR_list==j) = std(est_errors);
+        allerr_list(1, iter_index) = mean(est_errors);
+        allerr_list(2, iter_index) = std(est_errors);
+        allerr_list(3, iter_index) = rms(est_errors);
+
+        SNRs_list(1, iter_index) = j;
+
+%         devs_list(1, SNR_list==j) = std(est_errors);
     end
-    if numel(SNR_list) == numel(SNR_list1)
-        detailed_devs = devs_list(1, :);
-        detailed_SNR = SNR_list;
-    else
-        idx_idefiks = numel(detailed_devs(1, :)) + 1;
-        detailed_devs( idx_idefiks:numel(devs_list(1, :))+idx_idefiks-1 ) = devs_list(1, :);
-        detailed_SNR( idx_idefiks:numel(devs_list(1, :))+idx_idefiks-1 ) = SNR_list;
-    end
+%     if numel(SNR_list) == numel(SNR_list1)
+%         detailed_devs = devs_list(1, :);
+%         detailed_SNR = SNR_list;
+%     else
+%         idx_idefiks = numel(detailed_devs(1, :)) + 1;
+%         detailed_devs( idx_idefiks:numel(devs_list(1, :))+idx_idefiks-1 ) = devs_list(1, :);
+%         detailed_SNR( idx_idefiks:numel(devs_list(1, :))+idx_idefiks-1 ) = SNR_list;
+%     end
 end
 
-% range_temp = 1:find(detailed_SNR == SNR_list1(end));
-% range_temp1 = SNR_list1(1):0.5:SNR_list1(end);
-% detailed_devs1 = interp1(SNR_list1, detailed_devs(range_temp), range_temp1);
-% 
-% range_temp = find(detailed_SNR == SNR_list3(1)):find(detailed_SNR == SNR_list3(end));
-% range_temp3 = SNR_list3(1):0.5:SNR_list3(end);
-% detailed_devs3 = interp1(SNR_list3, detailed_devs(range_temp), range_temp3);
-% 
-% range_temp = find(detailed_SNR == SNR_list4(1)):find(detailed_SNR == SNR_list4(end));
-% range_temp4 = SNR_list4(1):0.5:SNR_list4(end);
-% detailed_devs4 = interp1(SNR_list4, detailed_devs(range_temp), range_temp4);
-% 
-% detailed_devs = [detailed_devs1, detailed_devs(find(detailed_SNR == SNR_list2(1)):find(detailed_SNR == SNR_list2(end))), detailed_devs3, detailed_devs4];
-% detailed_SNR = SNR_list1(1):0.5:SNR_list4(end);
+
+
+range_temp = 1:find(SNRs_list == SNR_list2(1));
+range_temp1 = SNR_list1(1):0.5:SNR_list2(1);
+detailed_errs1 = [interp1([SNR_list1, SNR_list2(1)], allerr_list(1, range_temp), range_temp1);
+    interp1([SNR_list1, SNR_list2(1)], allerr_list(2, range_temp), range_temp1);
+    interp1([SNR_list1, SNR_list2(1)], allerr_list(3, range_temp), range_temp1)];
+detailed_errs1 = detailed_errs1(:, 1:end-1);
+
+range_temp = find(SNRs_list == SNR_list2(end)):find(SNRs_list == SNR_list4(1));
+range_temp3 = SNR_list2(end):0.5:SNR_list4(1);
+detailed_errs3 = [interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(1, range_temp), range_temp3);
+    interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(2, range_temp), range_temp3);
+    interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(3, range_temp), range_temp3)];
+detailed_errs3 = detailed_errs3(:, 2:end-1);
+
+range_temp = find(SNRs_list == SNR_list4(1)):find(SNRs_list == SNR_list4(end));
+range_temp4 = SNR_list4(1):0.5:SNR_list4(end);
+detailed_errs4 = [interp1(SNR_list4, allerr_list(1, range_temp), range_temp4);
+    interp1(SNR_list4, allerr_list(2, range_temp), range_temp4);
+    interp1(SNR_list4, allerr_list(3, range_temp), range_temp4)];
+
+allerr_list = [detailed_errs1, allerr_list(:, find(SNRs_list == SNR_list2(1)):find(SNRs_list == SNR_list2(end))), detailed_errs3, detailed_errs4];
+SNRs_list = SNR_list1(1):0.5:SNR_list4(end);
+
+
+figure('Name', 'Mean Error to SNR');
+plot(SNRs_list, allerr_list(1, :)*10^(9)); %w ns
+title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(dev_iter),  'iteracji, ', type_name, corr_osval])
+xlabel('SNR [dB]')
+ylabel('Średnia wartość błędu [ns]')
 
 figure('Name', 'Deviations to SNR');
-plot(detailed_SNR, detailed_devs*10^(9)); %w ns
+plot(SNRs_list, allerr_list(2, :)*10^(9)); %w ns
 title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(dev_iter),  'iteracji, ', type_name, corr_osval])
 xlabel('SNR [dB]')
 ylabel('Odchylenie standardowe [ns]')
+
+figure('Name', 'RMS to SNR');
+plot(SNRs_list, allerr_list(3, :)*10^(9)); %w ns
+title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(dev_iter),  'iteracji, ', type_name, corr_osval])
+xlabel('SNR [dB]')
+ylabel('RMS [ns]')
 
 toc
