d313fd6cde65539c7df4e0c34761bad77a70eb69
szymsioH <01169288@pw.edu.pl>
2024-12-14 00:01:07 +0100

    Prepered MeshEnvirement, fixed most calculations (not on of xcorr)

--- MeshEnvirement.m
+++ MeshEnvirement.m
@@ -1,66 +1,116 @@
 clear workspace;
 
+tic
+
 %Wczytanie sygnału dvbt
 [signal_dvbt, fs, fc] = loadDVBTFunction('dvbt_signal.mat');
 
 %Wczytanie lokalizacji odbiorników
 [x_car, x_geo] = receiversPositionFunction();
 
+%ustawienia fminunc
+x0 = [0; 0];
+options = optimoptions('fminunc','Algorithm','quasi-newton','Display','off');
+
 %Prędkość propagacji sygnału
 c = 299792458;
 
-%Stworzenie siatki nadajników
-mesh_range = -30000:500:30000;
-[xsources, ysources] = meshgrid(mesh_range, mesh_range);
+os_type = 0; %rodzaj interpolacji
+os_value = 1;
+co_value = 1;
+iter_m = 5; %iteracje
 
-% snr_mesh = zeros(numel(ysources(:, 1), xsources(1, :)));
-% for j = 1:numel(ysources(:, 1))
-%     for i = 1:numel(xsources(1, :))
-%         trans = [xsources(1, i), ysources(j, 1)];
-%         rec = x_car(1:2, 2);
-%         d = abs(norm(rec-trans));
-%         SNR_dB = calcSnrFunction(c, d);
-%         snr_mesh(i, j) = SNR_dB;
-%     end
-% end
+%Stworzenie siatki nadajników
+mesh_range = -8000:1000:8000;
+[xsources, ysources] = meshgrid(mesh_range, mesh_range);
+ysources = flip(ysources);
+
+xsources = xsources(1, :);
+ysources = ysources(:, 1).';
+
+dev_mesh = zeros(numel(xsources), numel(ysources));
+mean_mesh = zeros(numel(xsources), numel(ysources));
+rms_mesh = zeros(numel(xsources), numel(ysources));
+
+for x = xsources
+    for y = ysources
+
+        R = [norm(x_car(1:2, 2).'-[x, y]);
+                norm(x_car(1:2, 3).'-[x, y]);
+                norm(x_car(1:2, 4).'-[x, y]);
+                norm(x_car(1:2, 5).'-[x, y])];
+
+        SNR_dB = calcSnrFunction(c, fc, R);
+        devs = zeros(1, 4);
+        for d = 1:4
+            devs(d) = getDevsFunction(signal_dvbt, os_type, os_value, co_value, fs, SNR_dB(d), R(d));
+        end
+
+        TOAs = R/c;
+        
+        est_errors = zeros(1, iter_m);
+        for i = 1:iter_m
+            %TDOAs: 1 - o-w, 2 - o-i, 3 - u-s
+            noises = [randi([-devs(1) devs(1)]);
+                randi([-devs(2) devs(2)]);
+                randi([-devs(3) devs(3)]);
+                randi([-devs(4) devs(4)]);];
+
+            TOAs = TOAs + noises;
+
+            TDOAs = [-TOAs(1)+TOAs(2); -TOAs(1)+TOAs(3); -TOAs(1)+TOAs(4)];
+            
+            [x_hat_opt, fval] = fminunc(@(x_hat) costFunctionLS_TDOA(x_hat, x_car, TDOAs, c), x0, options);
+
+            est_errors(i) = norm(x_hat_opt - [x; y]);
+        end
+        dev_mesh(xsources==x, ysources==y) = std(est_errors);
+        mean_mesh(xsources==x, ysources==y) = mean(est_errors);
+        rms_mesh(xsources==x, ysources==y) = rms(est_errors);
+    end
+end
+
+figure('Name', 'Mean Error');
+clim1 = [0 2000];
+imagesc(xsources, ysources, mean_mesh, clim1)
+colorbar;
+xlabel('x [m]')
+ylabel('y [m]')
+axis xy
+title('Błąd średni względem położenia nadajnika')
+hold on
+plot(x_car(1, 2:5).', x_car(2, 2:5).', '^k', 'MarkerSize', 8, 'MarkerFaceColor', 'yellow');
+
+toc
+
+% R = linspace(1, 100e3, 1000);
+% 
+% SNR_dB = calcSnrFunction(c, fc, R);
 % 
 % figure;
-% imagesc(xsources(1, :), ysources(:, 1), snr_mesh)
-% colorbar;
-% xlabel('m')
-% ylabel('m')
-% title('Wartości SNR dla położeń nadajnika względem Oblotu')
-% hold on
-% plot(0, 0, '^black', 'MarkerSize', 5, 'MarkerFaceColor', 'yellow')
+% plot(R/1e3, SNR_dB)
+% xlabel('R [km]')
+% ylabel('SNR [dB]')
+% grid on;
 
-R = linspace(1, 100e3, 1000);
-
-SNR_dB = calcSnrFunction(c, R);
-
-figure;
-plot(R/1e3, SNR_dB)
-xlabel('R [km]')
-ylabel('SNR [dB]')
-grid on;
-
-function SNR_dB = calcSnrFunction(c, R)
+function SNR_dB = calcSnrFunction(c, fc, R)
     %Dla Warszawa Raszyn:
     ERP_lin = 100000;
     ERP_dBW = 10*log10(ERP_lin);
     T0 = 2300;
     Gr = 1; % zysk anteny odbiorczej
-    f = 690*10^6;
+    f = fc;
     lamb = c/f;
     B = 8*10^6;
-    Nt_dBW = (-174 + 10*log10(B)) - 30;
-%     Lfs_dB = 20*log((4*pi*R)/(lamb));
-    Lfs_dB = 32.44 + 20*log10(f/10^6) + 20*log10(R/1000);
-    Pr_dB = ERP_dBW - Lfs_dB;
-    SNR_dB = Pr_dB - Nt_dBW;
+%     Nt_dBW = (-174 + 10*log10(B)) - 30;
+% %     Lfs_dB = 20*log((4*pi*R)/(lamb));
+%     Lfs_dB = 32.44 + 20*log10(f/10^6) + 20*log10(R/1000);
+%     Pr_dB = ERP_dBW - Lfs_dB;
+%     SNR_dB = Pr_dB - Nt_dBW;
 
-%     Psignal = ERP_lin./(4*pi*R.^2)*lamb^2*Gr/(4*pi);
-%     Pnoise = 1.380e-23*T0*B;
-%     SNR = Psignal/Pnoise;
-%     SNR_dB = 10*log10(SNR);
+    Psignal = ERP_lin./(4*pi*R.^2)*lamb^2*Gr/(4*pi);
+    Pnoise = 1.380e-23*T0*B;
+    SNR = Psignal/Pnoise;
+    SNR_dB = 10*log10(SNR);
 
 end
--- TestChamber.m
+++ TestChamber.m
@@ -1,227 +1,191 @@
 clear workspace;
 
-%Wczytanie sygnału dvbt
-[signal_dvbt, fs, fc] = loadDVBTFunction('dvbt_signal.mat');
-
-%Prędkość propagacji sygnału
-c = 299792458;
-
 tic
-%skrócenie sygnału do 2ms
-%sig_time = (numel(signal_dvbt)/double(fs))*1000; %in ms
 
-sig_time_new = numel(signal_dvbt)/50;
-
-signal = signal_dvbt(int64(numel(signal_dvbt)/3):int64(numel(signal_dvbt)/3 + sig_time_new - 1));
-
-%przesunięcie sygnałów
 distance = 1000; %odległość w m
-delay = distance/c; %opóźnienie w s
+os_type = 2; %rodzaj interpolacji
+os_value = 2;
+co_value = 2;
+iters = 50; %iteracje
 
-os_type = 0; %rodzaj interpolacji
-os_value = 1;
-co_value = 5;
-
-dev_iter = 50; %iteracje
-
-delay_samples = delay*double(fs); %opóźnienie w próbkach
-
-sig_drift = delay_samples - fix(delay_samples);
-
-% xq = 1 + sig_drift:1:numel(signal)-1+sig_drift;
-% 
-% signal_il = interp1(signal, xq);
-% 
-% signal2 = [zeros(1, round(delay_samples)), signal_il];
-
-signal2 = [zeros(1, round(delay_samples)), signal];
-signal = [signal, zeros(1, round(delay_samples))];
-
-type_name = '';
-corr_osval = '';
-
-switch os_type
-    case 1
-        %powielenie próbek sygnału
-        type_name = 'repelem';
-        signal = repelem(signal, os_value);
-        signal2 = repelem(signal2, os_value);
-    case 2
-        %interpolacja liniowa
-        type_name = 'interp1';
-        range = 1:(1/os_value):numel(signal);
-        signal = interp1(signal, range);
-        range2 = 1:(1/os_value):numel(signal2);
-        signal2 = interp1(signal2, range2);
-    case 3
-        %interpolacja fourierowska
-        type_name = 'interpfr';
-        signal = interpft(signal, os_value*numel(signal));
-        signal2 = interpft(signal2, os_value*numel(signal2));
-    case 4
-        %zwiększenie liczby próbek z dopasowaniem filtra dolnoprzepustowego
-        type_name = 'resample';
-        signal = resample(signal, double(os_value), 1);
-        signal2 = resample(signal2, double(os_value), 1);
-    otherwise
-        os_value = 1;
-end
-fs = fs * os_value;
-
-
-
-noise = (randn(1, numel(signal)) + 1i*randn(1, numel(signal)));
-noise2 = (randn(1, numel(signal2)) + 1i*randn(1, numel(signal2)));
-
-%dodanie szumu
-snr_check = snr(signal, noise);
-snr2_check = snr(signal2, noise2);
-
-SNR_eqalizer1 = snr_check; %wyrównanie poziomów sygnału i szumu
-SNR_eqalizer2 = snr2_check;
-
-% SNR_list1 = -30:2:-24;
-% SNR_list2 = -23.5:0.5:0.5;
-% SNR_list3 = 1:1:4;
-% SNR_list4 = 5:5:20;
-
-SNR_list1 = -30:2:-24;
-SNR_list2 = -23.5:0.5:0.5;
-SNR_list3 = 1:1:4;
-SNR_list4 = 5:5:160;
-
-iter_index = 0;
-
-allerr_list = zeros(3, numel([SNR_list1, SNR_list2, SNR_list3, SNR_list4])); %wiersze: 1 - mean, 2 - std, 3 - rms
-SNRs_list = zeros(1, numel(allerr_list(1, :)));
-
-for n = 1:4
-    switch n
-        case 1
-            SNR_list = SNR_list1;
-        case 2
-            SNR_list = SNR_list2;
-        case 3
-            SNR_list = SNR_list3;
-        case 4
-            SNR_list = SNR_list4;
-    end
-
-    for j = SNR_list
-        
-        iter_index = iter_index + 1;
-
-%         SNR1 = SNR_eqalizer1 + j;
-%         SNR2 = SNR_eqalizer2 + j;
-        SNR1 = j;
-        SNR2 = j;
-        
-        est_errors = zeros(1, dev_iter);
-        lag_errors = zeros(1, dev_iter);
-        for i = 1:dev_iter
-        
-            noise1 = (randn(1, numel(signal)) + 1i*randn(1, numel(signal)));
-            noise2 = (randn(1, numel(signal2)) + 1i*randn(1, numel(signal2)));
-
-            check1 = snr(signal, noise1);
-%             check2 = snr(signal2, noise2);
-
-            db2mag(-SNR_eqalizer1);
-%             db2mag(-SNR_eqalizer2);
-
-            signaln = signal.*db2mag(-(check1-j));
-            signal2n = signal2.*db2mag(-(check1-j));
-
-            check3 = snr(signaln, noise1);
-            check4 = snr(signal2n, noise2);
-            
-            signaln = signaln + noise1;
-            signal2n = signal2n + noise2;
-
-            check5 = snr(signaln, signal2n);
-            
-            % snr_value = snr(signal, noise1);
-            
-            %korelacja
-            [corrval, lag] = xcorr(signaln, signal2n);
-    
-            if co_value == 1
-                max_lag = -lag(corrval == max(corrval));
-            else
-                %nadpróbkowanie korelacji
-                corr_osval = [' ov sampl * ', num2str(co_value)];
-                rangecorr = min(lag):(1/co_value):max(lag);
-                corrval_itp = interp1(lag, corrval, rangecorr, 'spline');
-    
-                [max_corr, max_idx] = max(corrval_itp);
-                max_lag = -rangecorr(corrval_itp == max(corrval_itp));
-            end
-            
-            est_delay = double(max_lag)/(double(fs)); % wyestymowane opóźnienie w s
-            
-            est_errors(i) = abs(delay - est_delay); %błąd estymaty w sekundach
-        end
-    
-        allerr_list(1, iter_index) = mean(est_errors);
-        allerr_list(2, iter_index) = std(est_errors);
-        allerr_list(3, iter_index) = rms(est_errors);
-
-        SNRs_list(1, iter_index) = j;
-
-%         devs_list(1, SNR_list==j) = std(est_errors);
-    end
-%     if numel(SNR_list) == numel(SNR_list1)
-%         detailed_devs = devs_list(1, :);
-%         detailed_SNR = SNR_list;
-%     else
-%         idx_idefiks = numel(detailed_devs(1, :)) + 1;
-%         detailed_devs( idx_idefiks:numel(devs_list(1, :))+idx_idefiks-1 ) = devs_list(1, :);
-%         detailed_SNR( idx_idefiks:numel(devs_list(1, :))+idx_idefiks-1 ) = SNR_list;
-%     end
-end
-
-
-
-range_temp = 1:find(SNRs_list == SNR_list2(1));
-range_temp1 = SNR_list1(1):0.5:SNR_list2(1);
-detailed_errs1 = [interp1([SNR_list1, SNR_list2(1)], allerr_list(1, range_temp), range_temp1);
-    interp1([SNR_list1, SNR_list2(1)], allerr_list(2, range_temp), range_temp1);
-    interp1([SNR_list1, SNR_list2(1)], allerr_list(3, range_temp), range_temp1)];
-detailed_errs1 = detailed_errs1(:, 1:end-1);
-
-range_temp = find(SNRs_list == SNR_list2(end)):find(SNRs_list == SNR_list4(1));
-range_temp3 = SNR_list2(end):0.5:SNR_list4(1);
-detailed_errs3 = [interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(1, range_temp), range_temp3);
-    interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(2, range_temp), range_temp3);
-    interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(3, range_temp), range_temp3)];
-detailed_errs3 = detailed_errs3(:, 2:end-1);
-
-range_temp = find(SNRs_list == SNR_list4(1)):find(SNRs_list == SNR_list4(end));
-range_temp4 = SNR_list4(1):0.5:SNR_list4(end);
-detailed_errs4 = [interp1(SNR_list4, allerr_list(1, range_temp), range_temp4);
-    interp1(SNR_list4, allerr_list(2, range_temp), range_temp4);
-    interp1(SNR_list4, allerr_list(3, range_temp), range_temp4)];
-
-allerr_list = [detailed_errs1, allerr_list(:, find(SNRs_list == SNR_list2(1)):find(SNRs_list == SNR_list2(end))), detailed_errs3, detailed_errs4];
-SNRs_list = SNR_list1(1):0.5:SNR_list4(end);
-
+[allerr_list, SNRs_list, type_name, corr_osval] = getErrsToSnrs(distance, os_type, os_value, co_value, iters);
 
 figure('Name', 'Mean Error to SNR');
 plot(SNRs_list, allerr_list(1, :)*10^(9)); %w ns
-title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(dev_iter),  'iteracji, ', type_name, corr_osval])
+title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(iters),  'iteracji, ', type_name, corr_osval])
 xlabel('SNR [dB]')
 ylabel('Średnia wartość błędu [ns]')
 
 figure('Name', 'Deviations to SNR');
 plot(SNRs_list, allerr_list(2, :)*10^(9)); %w ns
-title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(dev_iter),  'iteracji, ', type_name, corr_osval])
+title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(iters),  'iteracji, ', type_name, corr_osval])
 xlabel('SNR [dB]')
 ylabel('Odchylenie standardowe [ns]')
 
 figure('Name', 'RMS to SNR');
 plot(SNRs_list, allerr_list(3, :)*10^(9)); %w ns
-title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(dev_iter),  'iteracji, ', type_name, corr_osval])
+title([num2str(distance), 'm, ', num2str(os_value), '*fs, ', num2str(iters),  'iteracji, ', type_name, corr_osval])
 xlabel('SNR [dB]')
 ylabel('RMS [ns]')
 
 toc
+
+function [allerr_list, SNRs_list, type_name, corr_osval] = getErrsToSnrs(distance, os_type, os_value, co_value, iters)
+
+    %Wczytanie sygnału dvbt
+    [signal_dvbt, fs, fc] = loadDVBTFunction('dvbt_signal.mat');
+    
+    %Prędkość propagacji sygnału
+    c = 299792458;
+    
+    %skrócenie sygnału do 2ms
+    sig_time_new = numel(signal_dvbt)/50;
+    
+    signal = signal_dvbt(int64(numel(signal_dvbt)/3):int64(numel(signal_dvbt)/3 + sig_time_new - 1));
+    
+    %przesunięcie sygnałów
+    delay = distance/c; %opóźnienie w s
+    
+    delay_samples = delay*double(fs); %opóźnienie w próbkach
+    
+    sig_drift = delay_samples - fix(delay_samples);
+    
+    signal2 = [zeros(1, round(delay_samples)), signal];
+    signal = [signal, zeros(1, round(delay_samples))];
+    
+    type_name = '';
+    corr_osval = '';
+    
+    switch os_type
+        case 1
+            %powielenie próbek sygnału
+            type_name = 'repelem';
+            signal = repelem(signal, os_value);
+            signal2 = repelem(signal2, os_value);
+        case 2
+            %interpolacja liniowa
+            type_name = 'interp1';
+            range = 1:(1/os_value):numel(signal);
+            signal = interp1(signal, range);
+            range2 = 1:(1/os_value):numel(signal2);
+            signal2 = interp1(signal2, range2);
+        case 3
+            %interpolacja fourierowska
+            type_name = 'interpfr';
+            signal = interpft(signal, os_value*numel(signal));
+            signal2 = interpft(signal2, os_value*numel(signal2));
+        case 4
+            %zwiększenie liczby próbek z dopasowaniem filtra dolnoprzepustowego
+            type_name = 'resample';
+            signal = resample(signal, double(os_value), 1);
+            signal2 = resample(signal2, double(os_value), 1);
+        otherwise
+            os_value = 1;
+    end
+    fs = fs * os_value;
+    
+    % SNR_list1 = -30:2:-24;
+    % SNR_list2 = -23.5:0.5:0.5;
+    % SNR_list3 = 1:1:4;
+    % SNR_list4 = 5:5:20;
+    
+    SNR_list1 = -30:2:-24;
+    SNR_list2 = -23.5:0.5:0.5;
+    SNR_list3 = 1:1:4;
+    SNR_list4 = 5:5:160;
+    
+    iter_index = 0;
+    
+    allerr_list = zeros(3, numel([SNR_list1, SNR_list2, SNR_list3, SNR_list4])); %wiersze: 1 - mean, 2 - std, 3 - rms
+    SNRs_list = zeros(1, numel(allerr_list(1, :)));
+    
+    for n = 1:4
+        switch n
+            case 1
+                SNR_list = SNR_list1;
+            case 2
+                SNR_list = SNR_list2;
+            case 3
+                SNR_list = SNR_list3;
+            case 4
+                SNR_list = SNR_list4;
+        end
+    
+        for j = SNR_list
+            iter_index = iter_index + 1;
+            est_errors = zeros(1, iters);
+            for i = 1:iters
+            
+                noise1 = (randn(1, numel(signal)) + 1i*randn(1, numel(signal)));
+                noise2 = (randn(1, numel(signal2)) + 1i*randn(1, numel(signal2)));
+    
+                check1 = snr(signal, noise1);
+    
+%                 db2mag(-SNR_eqalizer1);
+    
+                signaln = signal.*db2mag(-(check1-j));
+                signal2n = signal2.*db2mag(-(check1-j));
+    
+%                 check3 = snr(signaln, noise1);
+%                 check4 = snr(signal2n, noise2);
+                
+                signaln = signaln + noise1;
+                signal2n = signal2n + noise2;
+    
+%                 check5 = snr(signaln, signal2n);
+                
+                % snr_value = snr(signal, noise1);
+                
+                %korelacja
+                [corrval, lag] = xcorr(signaln, signal2n);
+        
+                if co_value == 1
+                    max_lag = -lag(corrval == max(corrval));
+                else
+                    %nadpróbkowanie korelacji
+                    corr_osval = [' ov sampl * ', num2str(co_value)];
+                    rangecorr = min(lag):(1/co_value):max(lag);
+                    corrval_itp = interp1(lag, corrval, rangecorr, 'spline');
+        
+%                     [max_corr, max_idx] = max(corrval_itp);
+                    max_lag = -rangecorr(corrval_itp == max(corrval_itp));
+                end
+                
+                est_delay = double(max_lag)/(double(fs)); % wyestymowane opóźnienie w s
+                
+                est_errors(i) = abs(delay - est_delay); %błąd estymaty w sekundach
+            end
+        
+            allerr_list(1, iter_index) = mean(est_errors);
+            allerr_list(2, iter_index) = std(est_errors);
+            allerr_list(3, iter_index) = rms(est_errors);
+    
+            SNRs_list(1, iter_index) = j;
+        end
+    end
+    
+    
+    
+    range_temp = 1:find(SNRs_list == SNR_list2(1));
+    range_temp1 = SNR_list1(1):0.5:SNR_list2(1);
+    detailed_errs1 = [interp1([SNR_list1, SNR_list2(1)], allerr_list(1, range_temp), range_temp1);
+        interp1([SNR_list1, SNR_list2(1)], allerr_list(2, range_temp), range_temp1);
+        interp1([SNR_list1, SNR_list2(1)], allerr_list(3, range_temp), range_temp1)];
+    detailed_errs1 = detailed_errs1(:, 1:end-1);
+    
+    range_temp = find(SNRs_list == SNR_list2(end)):find(SNRs_list == SNR_list4(1));
+    range_temp3 = SNR_list2(end):0.5:SNR_list4(1);
+    detailed_errs3 = [interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(1, range_temp), range_temp3);
+        interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(2, range_temp), range_temp3);
+        interp1([SNR_list2(end), SNR_list3, SNR_list4(1)], allerr_list(3, range_temp), range_temp3)];
+    detailed_errs3 = detailed_errs3(:, 2:end-1);
+    
+    range_temp = find(SNRs_list == SNR_list4(1)):find(SNRs_list == SNR_list4(end));
+    range_temp4 = SNR_list4(1):0.5:SNR_list4(end);
+    detailed_errs4 = [interp1(SNR_list4, allerr_list(1, range_temp), range_temp4);
+        interp1(SNR_list4, allerr_list(2, range_temp), range_temp4);
+        interp1(SNR_list4, allerr_list(3, range_temp), range_temp4)];
+    
+    allerr_list = [detailed_errs1, allerr_list(:, find(SNRs_list == SNR_list2(1)):find(SNRs_list == SNR_list2(end))), detailed_errs3, detailed_errs4];
+    SNRs_list = SNR_list1(1):0.5:SNR_list4(end);
+end
